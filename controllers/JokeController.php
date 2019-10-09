7cde42d71d4e374ceb5f7c9009af38a96823478d
takgukim <ceman08071039@gmail.com>
2019-10-09 18:58:12 +0900

    2019-10-09 final source upload. leetaeehe

--- controllers/JokeController.php
+++ controllers/JokeController.php
@@ -32,22 +32,20 @@
 
             $totalJokes = $this->jokesTable->total();
 
-            ob_start();
-            include_once __DIR__ . '/../templates/jokes.html.php';
-            $output = ob_get_clean();
-
-            return ['output'=> $output, 'title'=> $title];
+            return ['template'=> 'jokes.html.php',
+                    'title'=> $title,
+                    'variables'=> [
+                        'totalJokes'=> $totalJokes,
+                        'jokes'=> $jokes
+                    ]
+                ];
         }
 
         public function home()
         {
             $title = '인터넷 유머 세상';
 
-            ob_start();
-            include_once __DIR__ . '/../templates/home.html.php';
-            $output = ob_get_clean();
-
-            return ['output'=> $output, 'title'=> $title];
+            return ['template'=> 'home.html.php', 'title'=> $title];
         }
 
         public function delete()
@@ -75,11 +73,13 @@
 
                 $title = '유머 글 수정';
 
-                ob_start();
-                include_once __DIR__ . '/../templates/editjoke.html.php';
-                $output = ob_get_clean();
-
-                return ['output'=> $output, 'title'=> $title];
+                return [
+                    'template'=> 'editjoke.html.php',
+                    'title'=> $title,
+                    'variables'=>[
+                        'joke'=> $joke ?? null
+                    ]
+                ];
             }
         }
     }
--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <?php
+    function loadTemplate($templateFileName, $variables = []){
+        extract($variables);
+
+        ob_start();
+        include_once __DIR__ . '/../templates/' . $templateFileName;
+
+        return ob_get_clean();
+    }
+
     try {
-       include __DIR__ . '/../includes/DatabaseConnection.php';
-       include __DIR__ . '/../classes/DatabaseTable.php';
-       include __DIR__ . '/../controllers/JokeController.php';
+       include_once __DIR__ . '/../includes/DatabaseConnection.php';
+       include_once __DIR__ . '/../classes/DatabaseTable.php';
+       include_once __DIR__ . '/../controllers/JokeController.php';
 
        $jokesTable = new DataBaseTable($pdo, 'joke', 'id');
        $authorsTable = new DatabaseTable($pdo, 'author', 'id');
@@ -10,12 +19,18 @@
        $jokeController = new JokeController($jokesTable, $authorsTable);
 
        $action = $_GET['action'] ?? 'home';
+
        $page = $jokeController->$action();
 
        $title = $page['title'];
-       $output = $page['output'];
 
+       if (isset($page['variables'])) {
+           $output = loadTemplate($page['template'], $page['variables']);
+       } else {
+           $output = loadTemplate($page['template']);
+
+       }
     } catch (PDOException $e) {
         $output = '데이터베이스 서버에 접속 할 수 없습니다: '.$e->getMessage().', 위치: '.$e->getFile().':'.$e->getLine();
     }
-    include __DIR__ .'/../templates/layout.html.php';
+    include_once __DIR__ .'/../templates/layout.html.php';
