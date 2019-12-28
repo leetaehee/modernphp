a93b9339b4bb1c8cd1e8affa70ffee6170865505
leetaehee <lastride25@naver.com>
2019-12-28 21:59:58 +0900

    autoloader namespace use 작업완료 2019-12-28 이태희 어달해변 투썸플레이스

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
     try {
-      include __DIR__ . '/../classes/EntryPoint.php';
-      include __DIR__ . '/../classes/IjdbRoutes.php';
+      include __DIR__  . '/../includes/autoload.php';
 
       $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
-
-      $entryPoint = new EntryPoint($route, new IjdbRoutes());
+      $entryPoint = new \Hanbit\EntryPoint($route, new \Ijdb\IjdbRoutes());
       $entryPoint->run();
     } catch (PDOException $e) {
         $output = '데이터베이스 서버에 접속 할 수 없습니다: '.$e->getMessage().', 위치: '.$e->getFile().':'.$e->getLine();
